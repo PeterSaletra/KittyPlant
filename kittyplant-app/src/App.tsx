f1a2678dd2220c3a5cda0870f5d4d48d8af4b7d8
Asasinxd <peter.saletra@gmail.com>
2025-04-07 12:41:15 +0200

    Finished front page

--- kittyplant-app/src/App.tsx
+++ kittyplant-app/src/App.tsx
@@ -3,15 +3,21 @@ import Hero from './components/Hero'
 import Features from './components/Features'
 import ProductInfo from './components/ProductInfo'
 import Footer from './components/Footer'
+// import leafbottom from './assets/leaf-bottom.png'
+import leaftoplfet from './assets/leaftopleft.png'
+import leaftopright from './assets/leaftopright.png'
 
 function App() {
   return (
     <div className="app-container">
+      
       <Header />
       <Hero />
       <Features />
       <ProductInfo />
       <Footer />
+      <img src={leaftoplfet} alt="Leaf Top Left" className="leaf-decoration top-left"/>
+      <img src={leaftopright} alt="Leaf Top Right" className="leaf-decoration top-right"/>
     </div>
   )
 }
--- kittyplant-app/src/components/Footer.tsx
+++ kittyplant-app/src/components/Footer.tsx
@@ -1,8 +1,11 @@
 import '../styles/Footer.css';
+import leafbottomleft from '../assets/leafbottomleft.png'
+import leafbottomright from '../assets/leafbottomright.png'
 
 const Footer = () => {
   return (
     <footer className="footer">
+      <div className='footer-content'>
       <h2 className="contact-title">CONTACT US</h2>
       <div className="contact-info">
         <div className="contact-item">
@@ -12,10 +15,9 @@ const Footer = () => {
           <span className="contact-label">EMAIL:</span> info@kittyplant.io
         </div>
       </div>
-      <div className="decorative-elements">
-        <div className="decoration green"></div>
-        <div className="decoration pink"></div>
       </div>
+      <img src={leafbottomleft} alt="Leaf Bottom Left" className="leaf-decoration bottom-left"/>
+      <img src={leafbottomright} alt="Leaf Bottom Right" className="leaf-decoration bottom-right"/>
     </footer>
   )
 }
