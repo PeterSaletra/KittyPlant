1d63429ac236e6fc214ca35b875679f9e0ec8c6d
Asasinxd <peter.saletra@gmail.com>
2025-04-09 23:29:51 +0200

    Added redirect and webpage to connect to wifi

---
+++ esp32/include/WebPage.h
@@ -0,0 +1,103 @@
+const char* webpage = R"html(
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>KittyPlant WiFi Setup</title>
+  <meta name=""description" content="Połącz się ze swoim urządzeniem KittyPlant" />
+  <style>
+    body
+{
+    margin: 0;
+    background-color: #FAA3AD;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 20px;
+    font-family: "Antonio", sans-serif;
+    font-style: light;
+  
+}
+
+.header {
+    background-color: #7AA081;
+    color: white;
+    padding: 20px 30px;
+    border-radius: 20px 20px 20px 20px;
+    box-shadow: 0 6px 8px rgba(0, 0, 0, 0.1);
+    font-size: 20px;
+    text-align: center;
+  }
+
+  .form-container {
+    background-color: #FAF3E8;
+    padding: 30px 20px;
+    border-radius: 20px;
+    box-shadow: 0 6px 8px rgba(0, 0, 0, 0.1);
+    margin-top: 20px;
+    width: 90%;
+    max-width: 350px;
+    font-size: 20px;
+  }
+
+  label {
+    display: block;
+    margin-bottom: 5px;
+    color: #000;
+  }
+
+  input[type="text"], input[type="password"] {
+    width: 95%;
+    padding: 10px;
+    margin-bottom: 20px;
+    border: none;
+    border-radius: 20px;
+    background-color: #FFD4D4;
+    font-size: 16px;
+  }
+
+  button {
+    width: 50%;
+    padding: 12px;
+    border: none;
+    border-radius: 20px;
+    background-color: #A2235E;
+    color: white;
+    font-family: "Antonio", sans-serif;
+    font-style: bold;
+    font-size: 24px;
+    cursor: pointer;
+    margin: 0 auto;
+    display: block;
+  }
+
+  button:hover {
+    background-color: #A2235E;
+  }
+  </style>
+</head>
+<body>
+
+  <div class="header">
+    Connect me to your<br>WIFI
+  </div>
+
+  <div class="form-container">
+    <form action="/connect" method="POST">
+      <label for="ssid">Name</label>
+      <input type="text" id="ssid" name="ssid" placeholder="TYPE(SSID)" required>
+
+      <label for="password">Password</label>
+      <input type="password" id="password" name="password" placeholder="TYPE" required>
+
+      <button type="submit">CONNECT</button>
+    </form>
+  </div>
+
+</body>
+</html>
+)html";
+
+
--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -2,13 +2,27 @@
 #include <WiFi.h>
 #include <esp_wifi.h>
 #include <WiFiMulti.h>
+#include <WebServer.h>
+#include <DNSServer.h>
 #include <HTTPClient.h>
+#include "WebPage.h"
 #include "WiFiCredentials.h"
 
 #define PIN_RED    23 // GPIO23
 #define PIN_GREEN  22 // GPIO22
 #define PIN_BLUE   21 // GPIO21
 #define PIN_SENSOR A0 // SVP
+
+const char* ssidAP = "";
+const char* passwordAP = "";
+
+DNSServer dnsServer;
+WebServer server(80);
+
+String ssid;
+String password;
+
+bool gotCredentials = false;
 
 void readMacAddress(){
   uint8_t baseMac[6];
@@ -30,15 +44,15 @@ void setColor(int red, int green, int blue) {
 }
 
 void blinkColor(int red, int green, int blue, int delayTime = 500) {
-  setColor(red, green, blue); // color
+  setColor(red, green, blue);
   delay(delayTime);
-  setColor(0, 0, 0); // Off
+  setColor(0, 0, 0);
   delay(delayTime);
 }
 
 void signalNoWifiConnection() {
-  blinkColor(255, 0, 0); // Red
-  blinkColor(0, 0, 255); // Blue
+  blinkColor(255, 0, 0);
+  blinkColor(0, 0, 255);
   Serial.println("No Wi-Fi connection!");
   Serial.println("Reconnecting...");
   
@@ -62,18 +76,77 @@ void signalNoWifiConnection() {
   }
 }
 
+void hanndlePage(){
+  server.send(200, "text/html", webpage);   
+}
+
+void handleConnect(){
+  ssid = server.arg("ssid");
+  password = server.arg("password");
+
+  server.send(200, "text/html", "<h1>Connecting...</h1>");
+  delay(1000);
+  gotCredentials = true;
+}
+
 void setup() {
   Serial.begin(115200);
   pinMode(PIN_RED,   OUTPUT);
   pinMode(PIN_GREEN, OUTPUT);
   pinMode(PIN_BLUE,  OUTPUT);
 
+  WiFi.setHostname("kp-0001");
+  WiFi.mode(WIFI_AP_STA);
+
+  WiFi.softAP(ssidAP, passwordAP);
+  Serial.println("Access Point started");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.softAPIP());
+  dnsServer.start(53, "*", WiFi.softAPIP());
+
+  server.on("/", hanndlePage);
+  server.on("/connect", HTTP_POST, handleConnect);
+  server.on("/generate_204", []() {
+    server.sendHeader("Location", "/");
+    server.send(302, "text/plain", "");
+  });
+  
+  // Apple captive portal trigger
+  server.on("/hotspot-detect.html", []() {
+    server.send(200, "text/html", webpage);
+  });
+  
+  // Windows captive portal trigger
+  server.on("/ncsi.txt", []() {
+    server.send(200, "text/plain", "Microsoft NCSI");
+  });
+  server.on("/connecttest.txt", []() {
+    server.send(200, "text/plain", "This is not Microsoft");
+  });
+  
+  server.on("/redirect", []() {
+    server.sendHeader("Location", "/");
+    server.send(302, "text/plain", "");
+  });
+  server.begin();
+
+  while(!gotCredentials){
+    dnsServer.processNextRequest();
+    server.handleClient();
+    Serial.println("Waiting for credentials...");
+  }
+  server.stop();
+  dnsServer.stop();
+
+  WiFi.setAutoReconnect(true);
+  WiFi.persistent(true);
+  WiFi.mode(WIFI_STA);
   WiFi.begin(SSID, PASSWORD);
 
   readMacAddress();
 
   while (WiFi.status() != WL_CONNECTED) {
-    blinkColor(0, 0, 255); // Blue
+    blinkColor(0, 0, 255);
     Serial.print(".");
   }
   Serial.println("\nConnected to Wi-Fi!");
@@ -89,12 +162,11 @@ void loop() {
   if (WiFi.status() != WL_CONNECTED) {
     signalNoWifiConnection();
   } else {
-    int sensorValue = analogRead(PIN_SENSOR); // Read the sensor value
+    int sensorValue = analogRead(PIN_SENSOR);
     Serial.print("Sensor Value: ");
     Serial.println(sensorValue);
 
-    int realValue = 100 - map(sensorValue, 0, 4095, 0, 100); // Map to percentage (0-100%)
-    Serial.print("Real Value: ");
+    int realValue = 100 - map(sensorValue, 0, 4095, 0, 100);
     Serial.printf("%d%%\n", realValue);
     delay(1000); 
   }
